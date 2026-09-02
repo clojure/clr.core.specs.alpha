158fefccc187788e337ab8965ad5050dacc3623e
David Miller <dmiller2718@gmail.com>
2026-09-02 10:23:00 -0400

    Prepare release 0.5.82-beta2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojure.clr/core.specs.alpha "0.5.82-beta1"
+(defproject org.clojure.clr/core.specs.alpha "0.5.82-beta2"
   :description "Port of clojure.org/core.specs.alpha to ClojureCLR"
   :url "https://github.com/clojure/clr.core.specs.alpha"
   :license {:name "Eclipse Public License"
